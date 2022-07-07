9a6a41b28d8595242e711bf17cf2e635548a489e
Dinesh Bala <baladnes@gmail.com>
2022-07-07 07:29:43 +0545

    Rendering list and key prop

--- react-concepts/src/App.jsx
+++ react-concepts/src/App.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import Person from "./components/Person";
 
+const personList = [
+  {
+    id: 1,
+    name: "John",
+    age: 30,
+  },
+  {
+    id: 2,
+    name: "Jane",
+    age: 25,
+  },
+  {
+    id: 3,
+    name: "Jack",
+    age: 20,
+  },
+  {
+    id: 4,
+    name: "Jack",
+    age: 20,
+  },
+];
+
 const App = () => {
   return (
-    <div>
-      <Person name="Ram" age={20} />
-      <Person name="Hari" age={22} />
-      <Person name="Alex" age={28} />
+    <div className="container">
+      {personList.map((person, index) => (
+        <Person key={person.id} name={person.name} age={person.age} />
+      ))}
     </div>
   );
 };
